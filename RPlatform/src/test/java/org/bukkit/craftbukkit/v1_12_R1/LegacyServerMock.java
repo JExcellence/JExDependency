f7bc0405a26b18c9203c4cfc85a15a246b31b3c2
CaptainPax <101286587+Captainpax@users.noreply.github.com>
2025-10-21 15:28:44 -0700

    test: cover server environment detection

---
+++ RPlatform/src/test/java/org/bukkit/craftbukkit/v1_12_R1/LegacyServerMock.java
@@ -0,0 +1,11 @@
+package org.bukkit.craftbukkit.v1_12_R1;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+
+/**
+ * Legacy server mock packaged to emulate older CraftBukkit implementations so
+ * {@link com.raindropcentral.rplatform.version.ServerEnvironment} marks the
+ * instance as non-modern when parsing the package identifier.
+ */
+public class LegacyServerMock extends ServerMock {
+}
