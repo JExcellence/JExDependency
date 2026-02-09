e5340daffce52329245a8f6d087ead14bd2c8a52
ItsRainingHP <brent.coleman@live.com>
2026-02-08 17:21:33 -0800

    Add boilerplate RDS code

---
+++ RDS/src/main/java/com/raindropcentral/rds/configs/ConfigSection.java
@@ -0,0 +1,11 @@
+package com.raindropcentral.rds.configs;
+
+import de.jexcellence.configmapper.sections.AConfigSection;
+import de.jexcellence.gpeee.interpreter.EvaluationEnvironmentBuilder;
+
+public class ConfigSection extends AConfigSection {
+
+    public ConfigSection(EvaluationEnvironmentBuilder baseEnvironment) {
+        super(baseEnvironment);
+    }
+}
